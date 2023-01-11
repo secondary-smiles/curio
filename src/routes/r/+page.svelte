e9865dd3fa29514e0eea29e6e4fba51bcdd05610
secondary-smiles <shavkinder@gmail.com>
2023-01-11 10:33:37 -0500

    Style results

--- src/routes/r/+page.svelte
+++ src/routes/r/+page.svelte
@@ -4,16 +4,41 @@
 	import Word from '$lib/elem/Word.svelte';
 
 	export let data: PageData;
+
+	let resultString = 'results';
+	$: resultString = data.words.length == 1 ? 'result' : 'results';
 </script>
 
 <main>
 	<div id="title">
 		<h2 id="result-title">Results for '{data.query}'</h2>
+		<p id="result-count">{data.words.length} {resultString}</p>
 	</div>
 
 	<div id="results">
 		{#each data.words as word}
-			<Word {word} />
+			<div class="result-word">
+				<Word {word} />
+			</div>
 		{/each}
 	</div>
 </main>
+
+<style>
+	#title {
+		text-align: center;
+	}
+
+	#result-title {
+		margin-bottom: 0;
+	}
+
+	#result-count {
+		margin-top: 0;
+		color: var(--accent-color);
+	}
+
+	.result-word {
+		padding-top: 10%;
+	}
+</style>
