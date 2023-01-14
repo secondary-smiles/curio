25b6447476d1cf076004a7b9c38d3fdb96d673c0
secondary-smiles <shavkinder@gmail.com>
2023-01-14 13:19:40 -0500

    Style submit page

--- src/lib/elem/SubmitWord.svelte
+++ src/lib/elem/SubmitWord.svelte
@@ -10,16 +10,18 @@
 		<h2>Submit a Word</h2>
 	</div>
 	<form id="word-form">
-		<input type="text" placeholder="word.." />
+		<input type="text" placeholder="word.." autocomplete="off" />
 
 		<select bind:value={selected}>
-			<option value="" disabled selected>select a word type</option>
+			<option value={null} disabled selected>select a word type</option>
 			{#each wordMap as wordType}
 				<option value={wordType}>
 					{wordType.type}
 				</option>
 			{/each}
 		</select>
+		<textarea placeholder="word definition.." />
+		<button id="submit-button" type="submit">submit</button>
 	</form>
 </main>
 
@@ -32,5 +34,19 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+	}
+
+	#word-form > * {
+		margin: 5px 0;
+	}
+
+	input {
+		width: 15%;
+
+		text-align: center;
+	}
+
+	textarea {
+		width: 25%;
 	}
 </style>
