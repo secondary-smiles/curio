edd9dfba452116f9af1a15b2bc2b2fa9ccd43f6b
secondary-smiles <shavkinder@gmail.com>
2023-01-28 18:07:27 -0500

    Add title and meta to random words page

--- src/routes/a/+page.svelte
+++ src/routes/a/+page.svelte
@@ -6,6 +6,11 @@
 	export let data: PageData;
 </script>
 
+<svelte:head>
+	<title>random words - The Curio</title>
+	<meta name="description" content="The Curio - Random words from across the site" />
+</svelte:head>
+
 <main>
 	{#each data.words as word}
 		<Word {word} />
@@ -14,5 +19,9 @@
 		<p class="subtext">no words exist.</p>
 	{/if}
 
-	<p class="subtext">this random function sucks, if you want you can improve it on <a href="https://github.com/secondary-smiles/curio">github</a></p>
+	<p class="subtext">
+		this random function sucks, if you want you can improve it on <a
+			href="https://github.com/secondary-smiles/curio">github</a
+		>
+	</p>
 </main>
